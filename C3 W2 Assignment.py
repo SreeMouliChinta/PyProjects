ce5bcfe7f39c59a396c21cdf747a6eb3e786563e
SreeMouliChinta <64743534+SreeMouliChinta@users.noreply.github.com>
2020-05-06 08:36:11 +0530

    Update C3 W2 Assignment.py

--- C3 W2 Assignment.py
+++ C3 W2 Assignment.py
@@ -2,15 +2,5 @@
 #looking for a regular expression of '[0-9]+' and then converting the extracted strings to integers
 #and summing up the integers.
 
-fname = input('Enter file name: ')
-fhand = open(fname)
-list = list()
-sum = 0
-
 import re
-for line in fhand :
-    extract = re.findall('([0-9]+)',line)
-    for num in extract :
-        sum = sum + float(num)
-
-print(int(sum))
+print( sum( [ int(x) for x in re.findall('[0-9]+',open(input("Enter file name :")).read()) ] ) )
